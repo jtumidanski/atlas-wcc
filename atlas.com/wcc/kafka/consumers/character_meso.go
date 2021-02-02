089027d87f5a500c0a2b2f90bc24a9185928a968
Justin Tumidanski <jtumidanski@gmail.com>
2021-02-02 18:53:56 -0500

    Add support for gaining meso, items, and nx.

--- atlas.com/wcc/kafka/consumers/character_meso.go
+++ atlas.com/wcc/kafka/consumers/character_meso.go
@@ -1,1 +1,33 @@
 package consumers
+
+import (
+   "atlas-wcc/socket/response/writer"
+   "log"
+)
+
+type characterMesoEvent struct {
+   CharacterId uint32 `json:"characterId"`
+   Gain        uint32 `json:"gain"`
+}
+
+func CharacterMesoEventCreator() EmptyEventCreator {
+   return func() interface{} {
+      return &characterMesoEvent{}
+   }
+}
+
+func HandleCharacterMesoEvent() ChannelEventProcessor {
+   return func(l *log.Logger, wid byte, cid byte, e interface{}) {
+      if event, ok := e.(*characterMesoEvent); ok {
+         as := getSessionForCharacterId(event.CharacterId)
+         if as == nil {
+            l.Printf("[ERROR] unable to locate session for character %d.", event.CharacterId)
+            return
+         }
+         (*as).Announce(writer.WriteShowMesoGain(event.Gain, false))
+         (*as).Announce(writer.WriteEnableActions())
+      } else {
+         l.Printf("[ERROR] unable to cast event provided to handler [HandleCharacterMesoEvent]")
+      }
+   }
+}
--- atlas.com/wcc/kafka/consumers/nx_picked_up.go
+++ atlas.com/wcc/kafka/consumers/nx_picked_up.go
@@ -1,1 +1,36 @@
 package consumers
+
+import (
+   "atlas-wcc/socket/response/writer"
+   "fmt"
+   "log"
+)
+
+const nxGainFormat = "You have earned #e#b%d NX#k#n."
+
+type nxPickedUpEvent struct {
+   CharacterId uint32 `json:"characterId"`
+   Gain        uint32 `json:"gain"`
+}
+
+func NXPickedUpEventCreator() EmptyEventCreator {
+   return func() interface{} {
+      return &nxPickedUpEvent{}
+   }
+}
+
+func HandleNXPickedUpEvent() ChannelEventProcessor {
+   return func(l *log.Logger, wid byte, cid byte, e interface{}) {
+      if event, ok := e.(*nxPickedUpEvent); ok {
+         as := getSessionForCharacterId(event.CharacterId)
+         if as == nil {
+            l.Printf("[ERROR] unable to locate session for character %d.", event.CharacterId)
+            return
+         }
+         (*as).Announce(writer.WriteHint(fmt.Sprintf(nxGainFormat, event.Gain), 300, 10))
+         (*as).Announce(writer.WriteEnableActions())
+      } else {
+         l.Printf("[ERROR] unable to cast event provided to handler [HandleNXPickedUpEvent]")
+      }
+   }
+}
